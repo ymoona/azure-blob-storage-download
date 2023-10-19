68ece288803cf23e0b4ce951c0c42d59e9fc41f0
ymoona <hans@ymoona.com>
2023-10-19 19:35:43 +0200

    Update documentation and cleanup.

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,34 +12,21 @@ if [[ -z "$INPUT_CONTAINER_NAME" ]]; then
   exit 1
 fi
 
-CONNECTION_METHOD=""
-
-if [[ -n "$INPUT_CONNECTION_STRING" ]]; then
-  CONNECTION_METHOD="--connection-string $INPUT_CONNECTION_STRING"
-elif [[ -n "$INPUT_SAS_TOKEN" ]]; then
-  if [[ -n "$INPUT_ACCOUNT_NAME" ]]; then
-    CONNECTION_METHOD="--sas-token $INPUT_SAS_TOKEN --account-name $INPUT_ACCOUNT_NAME"
-  else
-    echo "account_name is required if using a sas_token. account_name is not set. Quitting."
-    exit 1
-  fi
-else
-  echo "either connection_string or sas_token are required and neither is set. Quitting."
+if [[ -z "$INPUT_CONNECTION_STRING" ]]; then
+  echo "connection string is not set. Quitting."
   exit 1
 fi
 
-ARG_OVERWRITE=""
-if [[ -n ${INPUT_OVERWRITE} ]]; then
-  ARG_OVERWRITE="--overwrite true"
+if [[ -z "$INPUT_FILE" ]]; then
+  echo "file is not set. Quitting."
+  exit 1
 fi
 
-EXTRA_ARGS=""
-if [[ -n ${INPUT_EXTRA_ARGS} ]]; then
-  EXTRA_ARGS=${INPUT_EXTRA_ARGS}
-fi
+
+
+CONNECTION_METHOD=""
+
+CONNECTION_METHOD="--connection-string $INPUT_CONNECTION_STRING"
 
 VERB="download"
 az storage blob ${VERB} ${CONNECTION_METHOD} --container-name ${INPUT_CONTAINER_NAME} --file ${INPUT_SOURCE_DIR}/${INPUT_FILE} --name ${INPUT_FILE}
-
-pwd 
-ls -hal
